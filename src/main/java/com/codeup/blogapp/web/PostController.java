f7bd7a097ec46f01eec65acbad8dabaf539155dd
stephen <stephen.h.nguyen.19@gmail.com>
2021-07-27 12:12:05 -0500

    adds getPosts

--- src/main/java/com/codeup/blogapp/web/PostController.java
+++ src/main/java/com/codeup/blogapp/web/PostController.java
@@ -1,8 +1,18 @@
 package com.codeup.blogapp.web;
 
 
+import com.codeup.blogapp.data.Post;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostController {
+
+
+    @GetMapping
+    public String getPosts(){
+
+
+        return Post;
+    }
 }
