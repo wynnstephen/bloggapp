e4a643284432a8571a56af50017ea496e090abb6
stephen <stephen.h.nguyen.19@gmail.com>
2021-07-29 09:50:38 -0500

    adds post

--- src/main/java/com/codeup/blogapp/web/PostController.java
+++ src/main/java/com/codeup/blogapp/web/PostController.java
@@ -3,16 +3,34 @@ package com.codeup.blogapp.web;
 
 import com.codeup.blogapp.data.Post;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping(value = "/api/posts", headers = "accept=application/json")
 public class PostController {
 
 
     @GetMapping
-    public String getPosts(){
-
-
-        return Post;
+    private List<Post> getPosts() {
+        return new ArrayList<Post>() {{
+            add(new Post(1L, "A new Post", "this is a brilliant post. 10/10"));
+            add(new Post(1L, "A new Post", "this is an even more brilliant post. 10/10"));
+            add(new Post(1L, "A new Post", "this is an even even more brilliant post. 10/10"));
+        }};
     }
+
+//    @PostMapping
+//    private void postPosts(){
+//
+//    }
+//
+//    @PostMapping
+//    private void createPosts(){
+//
+//    }
 }
