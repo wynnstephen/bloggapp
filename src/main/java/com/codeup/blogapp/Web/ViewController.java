0f2b2f6e9dfe32e594b95c95d64912b6c4f5df0e
stephen <stephen.h.nguyen.19@gmail.com>
2021-08-04 11:57:55 -0500

    adds ViewController

--- src/main/java/com/codeup/blogapp/Web/ViewController.java
+++ src/main/java/com/codeup/blogapp/Web/ViewController.java
@@ -1,4 +1,18 @@
 package com.codeup.blogapp.Web;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ViewController {
+
+
+    @RequestMapping({"/", "/about", "/posts", "/login", "/home"})
+    public String showView(){
+
+        return "forward:/index.html";
+
+
+    }
 }
