e3cc0340d2d27d35804f9c254310d8856c47c0f7
stephen <stephen.h.nguyen.19@gmail.com>
2021-08-19 10:30:46 -0500

    adds code

--- src/main/java/com/codeup/blogapp/data/Post.java
+++ src/main/java/com/codeup/blogapp/data/Post.java
@@ -1,16 +1,44 @@
 package com.codeup.blogapp.data;
 
+import org.apache.catalina.User;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name="posts")
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable=false, length=120)
     private String title;
+
+    @Column(nullable=false)
     private String content;
 
-    public Post(Long id, String title, String content) {
+    private User user;
+
+    private Collection<Catagory> categories;
+
+
+    public Post(Long id, String title, String content, User user, Collection<Catagory>) {
         this.id = id;
         this.title = title;
         this.content = content;
+        this.user = user;
+        this.categories = catagories;
     }
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+
+    }
+
+    public Post(){}
 
     public Long getId() {
         return id;
